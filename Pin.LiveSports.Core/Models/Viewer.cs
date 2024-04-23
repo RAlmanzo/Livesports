da94aca9c1cac08b934ebaf579efde4aec08e905
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-23 18:44:10 +0200

    Create new model viewer

---
+++ Pin.LiveSports.Core/Models/Viewer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.LiveSports.Core.Models
+{
+    public class Viewer
+    {
+        public string ConnectionId { get; set; }
+        public string Name { get; set; }
+    }
+}
