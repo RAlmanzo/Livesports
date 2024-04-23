289c9a5e18a1dd7ac49d1569741b4c410975cf25
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-23 18:44:32 +0200

    Create interface igameservice and set contract

---
+++ Pin.LiveSports.Core/Services/Interfaces/IGameService.cs
@@ -0,0 +1,16 @@
+﻿using Pin.LiveSports.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.LiveSports.Core.Services.Interfaces
+{
+    public interface IGameService
+    {
+        void AddViewer(Viewer viewer);
+        List<Viewer> GetAllViewers();
+    }
+}
