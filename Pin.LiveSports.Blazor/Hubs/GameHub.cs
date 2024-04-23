1879b660cd236d67428afc59f9e66ec6b9d6ae9a
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-23 18:40:59 +0200

    Create new hub gamehub and edit method newviewerjoined

---
+++ Pin.LiveSports.Blazor/Hubs/GameHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Pin.LiveSports.Core.Models;
+using Pin.LiveSports.Core.Services;
+using Pin.LiveSports.Core.Services.Interfaces;
+using System.Numerics;
+
+namespace Pin.LiveSports.Blazor.Hubs
+{
+    public class GameHub : Hub
+    {
+        private readonly IGameService _gameService;
+
+        public GameHub(IGameService viewerService)
+        {
+            _gameService = viewerService;
+        }
+
+        public async Task NewViewerJoined(string viewerName)
+        {
+            var newViewer = new Viewer
+            {
+                ConnectionId = Context.ConnectionId,
+                Name = viewerName,
+            };
+
+            _gameService.AddViewer(newViewer);
+            await Clients.Others.SendAsync("NewViewerAddedToViewerList", viewerName);
+        }
+    }
+}
