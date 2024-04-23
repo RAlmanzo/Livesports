309a5708e6aae8580fa75fdd16b7600acb5639be
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-04-23 18:44:56 +0200

    Create new class gameservice and impliment igameservice

---
+++ Pin.LiveSports.Core/Services/GameService.cs
@@ -0,0 +1,31 @@
+﻿using Pin.LiveSports.Core.Models;
+using Pin.LiveSports.Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.LiveSports.Core.Services
+{
+    public class GameService : IGameService
+    {
+        private List<Viewer> viewers;
+
+        public GameService()
+        {
+            viewers = new List<Viewer>();
+        }
+
+        public void AddViewer(Viewer viewer)
+        {
+            viewers.Add(viewer);
+        }
+
+        public List<Viewer> GetAllViewers()
+        {
+            return viewers;
+        }
+    }
+}
