47bec6b3774c8987a48964c9f944f1000b4fad52
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-30 17:04:04 +0200

    Create new model player

---
+++ Pin.LiveSports.Core/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.LiveSports.Core.Models
+{
+    public class Player
+    {
+        public string Position {  get; set; }
+        public string FullName { get; set; }
+        public string Country { get; set; }
+        public string CountryFlagIcon { get; set; }
+        public List<GameRound> GameRounds { get; set; }
+    }
+}
