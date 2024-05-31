3de494d8111a15a4bd40cf9656217c2cdf76c517
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-05-31 16:12:06 +0200

    Create model class game

---
+++ Pin.LiveSports.Core/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.LiveSports.Core.Models
+{
+    public class Game
+    {
+        public List<Player> Players { get; set; }
+        public bool IsStarted { get; set; }
+    }
+}
