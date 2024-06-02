9b9b6bdcf545bf4e5a0bbe3a0ef8b941e1a4c0df
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-06-02 19:47:30 +0200

    Create model class gameround

---
+++ Pin.LiveSports.Core/Models/GameRound.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.LiveSports.Core.Models
+{
+    public class GameRound
+    {
+        public string RoundNumber { get; set; }
+        public TimeSpan RoundTime { get; set; }
+        public TimeSpan PenaltyTime { get; set; }
+    }
+}
