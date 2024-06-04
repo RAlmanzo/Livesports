7d6fba37f5958565ba9114177b27d2ab6b47d31a
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-06-04 15:02:12 +0200

    Create interface ifileservice

---
+++ Pin.LiveSports.Blazor/Services/Interfaces/IFileService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Pin.LiveSports.Blazor.Services.Interfaces
+{
+    public interface IFileService
+    {
+        Task<string> LoadFile (IBrowserFile file);
+    }
+}
