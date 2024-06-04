4cc6fb2bc7ee0957d7a8757d9d2f946e05ddb264
Almanzo Rosseel <almanzo.rosseel@student.howest.be>
2024-06-04 12:52:47 +0200

    Add property image to report model

--- Pin.LiveSports.Core/Models/Report.cs
+++ Pin.LiveSports.Core/Models/Report.cs
@@ -11,5 +11,6 @@ namespace Pin.LiveSports.Core.Models
         public string Message { get; set; }
         public int PortNumber { get; set; }
         public string PenaltyIcon { get; set; }
+        public string Image {  get; set; }
     }
 }
